4b41ac5655ab23681441955449a738e236e60108
Narumi Abe <smile.narukkory@gmail.com>
2022-07-30 03:19:17 +0000

    [Add]編集機能、ポスト画像

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,24 +1,30 @@
 class PostsController < ApplicationController
   def new
+    @post = Post.new
   end
 
   def create
-    @post=Post.new(post_params)
+    @post = Post.new(post_params)
     @post.save
     redirect_to post_path(@post)
   end
 
   def index
-    @posts=Post.all
+    @posts = Post.all
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    post = Post.find(params[:id])
+    post.update(post_params)
+    redirect_to post_path(post)
   end
 
   def destroy
@@ -26,6 +32,6 @@ class PostsController < ApplicationController
 
   private
   def post_params
-    params.permit(:content)
+    params.require(:post).permit(:image, :content)
   end
 end
